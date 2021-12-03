e8fd6f91ee5d327c93db75ffcfa60bad04da99fe
Pércio Reinert <percioreinert@gmail.com>
2021-12-03 15:32:05 -0300

    second try

--- src/main/java/br/pucrs/t2/sales/adaptadores/controllers/SalesController.java
+++ src/main/java/br/pucrs/t2/sales/adaptadores/controllers/SalesController.java
@@ -1,5 +1,6 @@
 package br.pucrs.t2.sales.adaptadores.controllers;
 
+import br.pucrs.t2.sales.negocio.entities.ItemEstoqueDTO;
 import br.pucrs.t2.sales.negocio.entities.Venda;
 import br.pucrs.t2.sales.negocio.services.SalesService;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +15,32 @@ public class SalesController {
         this.salesService = salesService;
     }
 
+    @GetMapping("/autorizacao")
+    @CrossOrigin(origins = "*")
+    public boolean podeVender(@RequestParam final long codProd,
+                              @RequestParam final int qtdade) {
+        return salesService.podeVender(codProd, qtdade);
+    }
+
+    @PostMapping("/confirmacao")
+    @CrossOrigin(origins = "*")
+    public boolean confirmaVenda(@RequestBody final ItemEstoqueDTO[] itens) {
+        // for(ItemEstoque item : itens){
+        //   sEstoque.save(item);
+        // }
+        salesService.confirmaVenda(itens);
+        return true;
+    }
+
     @GetMapping("/historico")
     @CrossOrigin(origins = "*")
     public Iterable<Venda> vendasEfetuadas() {
         return salesService.vendasEfetuadas();
     }
 
-    @PutMapping("/salvar")
+    @PostMapping("/subtotal")
     @CrossOrigin(origins = "*")
-    public void salvarVenda(@RequestBody Venda venda) {
-        salesService.salvarVenda(venda);
+    public Integer[] calculaSubtotal(@RequestBody final ItemEstoqueDTO[] itens) {
+        return salesService.calculaSubtotal(itens);
     }
 }
--- src/main/java/br/pucrs/t2/sales/negocio/services/SalesService.java
+++ src/main/java/br/pucrs/t2/sales/negocio/services/SalesService.java
@@ -2,35 +2,117 @@ package br.pucrs.t2.sales.negocio.services;
 
 import br.pucrs.t2.sales.adaptadores.repositories.SalesRepository;
 import br.pucrs.t2.sales.negocio.entities.ItemEstoqueDTO;
+import br.pucrs.t2.sales.negocio.entities.ItemVenda;
 import br.pucrs.t2.sales.negocio.entities.Venda;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Service
 public class SalesService {
 
     private SalesRepository salesRepository;
+    private static long IMPOSTO = 10L;
 
     public SalesService(SalesRepository salesRepository) {
         this.salesRepository = salesRepository;
+    }
+
+    public Integer[] calculaSubtotal(ItemEstoqueDTO[] itens) {
+        int subtotal = 0;
+        int imposto = 0;
+        for (ItemEstoqueDTO it : itens) {
+            validaItemEstoque(it, 0);
+            int quantidade = it.getQuantidade();
+            // TODO: fazer a chamada para o serviço de estoque
+            it = estoqueService.getItemEstoque(it.getCodigo());
+            validaItemEstoque(it, quantidade);
+            subtotal += (int) (it.getProduto().getPreco() * quantidade);
+
+        }
+        imposto = (int)(subtotal * (IMPOSTO / 100.0));
+        final Integer[] resp = new Integer[3];
+        resp[0] = subtotal;
+        resp[1] = imposto;
+        resp[2] = subtotal + imposto;
+        return resp;
+    }
+
+    public boolean confirmaVenda(ItemEstoqueDTO[] itens) {
+        ArrayList<ItemVenda> listaItemVenda = new ArrayList<>();
+        ArrayList<ItemEstoqueDTO> listaItemEstoque = new ArrayList<>();
+        Venda venda = new Venda(LocalDateTime.now(), listaItemVenda);
+        for (ItemEstoqueDTO item : itens) {
+
+            if(item.getProduto() == null)
+                return false;
+
+            long codigoProduto = item.getCodigo();
+            int quantidade = item.getQuantidade();
+
+            if(quantidade <= 0)
+                return false;
+
+            ItemEstoqueDTO itemEstoque = listaItemEstoque.stream().filter(y -> y.getCodigo() == codigoProduto).findFirst().get().orElseThrow(null);
+
+            if(itemEstoque == null || itemEstoque.getProduto() == null){
+                // TODO: fazer a chamada para o serviço de estoque
+                itemEstoque = estoqueService.getItemEstoque(codigoProduto);
+
+                if(itemEstoque == null || itemEstoque.getProduto() == null)
+                    return false;
+
+                listaItemEstoque.add(itemEstoque);
+            }
+
+            quantidade = itemEstoque.getQuantidade() - quantidade;
+
+            if(quantidade >= 0)
+                itemEstoque.setQuantidade(quantidade);
+            else
+                return false;
+
+            ItemVenda itemLista = listaItemVenda.stream().filter(x -> x.getCodigo() == codigoProduto).findFirst().get().orElseThrow(null);
+
+            if(itemLista != null)
+                itemLista.setQtdProduto(quantidade);
+            else{
+                itemLista = new ItemVenda(quantidade, itemEstoque.getProduto().getPreco(), IMPOSTO, itemEstoque.getProduto(), venda);
+                listaItemVenda.add(itemLista);
+            }
+        }
+        salesRepository.save(venda);
+        // TODO: fazer a chamada para o serviço de estoque
+        estoqueService.save(listaItemEstoque);
+        return true;
     }
 
     public Iterable<Venda> vendasEfetuadas() {
         return salesRepository.findAll();
     }
 
-    public void salvarVenda(Venda venda) {
-        salesRepository.save(venda);
-    }
-
     public boolean podeVender(long codProd, int qtdade){
         if(qtdade <= 0)
             return false;
-        //ver como faz a chamada para o microserviço de estoque
+        // TODO: fazer a chamada para o serviço de estoque
         ItemEstoqueDTO item = estoqueService.getItemEstoque(codProd);
         if(item == null || qtdade > item.getQuantidade() || item.getQuantidade() - qtdade < 0)
             return false;
 
         item.setQuantidade(item.getQuantidade()-qtdade);
         return true;
+    }
+
+    private void validaItemEstoque(ItemEstoqueDTO itemEstoque, int quantidade){
+        String errorMessage = "";
+
+        if (itemEstoque == null)
+            errorMessage = "item inválido";
+        else if(itemEstoque.getQuantidade() - quantidade < 0)
+            errorMessage = "quantidade inválida";
+
+        if(!errorMessage.isEmpty())
+            throw new IllegalArgumentException(errorMessage);
     }
 }
